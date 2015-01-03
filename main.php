8e6da6dc7c25311e9edffefa342dcb82c45c54f5
bercik999 <bercik999@gmail.com>
2015-01-03 18:57:07 +0100

    more friendly result image

--- main.php
+++ main.php
@@ -10,13 +10,13 @@ include 'RandomGenerator.php';
 include 'ResultImage.php';
 
 //How many hotdogs we will throw
-define('throws_count', 10000);
+define('throws_count', 1000);
 //How big result image will be
 define('img_scale', 2);
 //how wide image will be compared to single hotdog
-define('img_width_multiplier', 30);
+define('img_width_multiplier', 10);
 //how many steps will be in final image
-define('lines_count', 10);
+define('lines_count', 30);
 //use random numbers from random.org - requires cUrl
 define('use_random_org', false);
 
