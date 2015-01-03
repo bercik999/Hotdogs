500771237704bbf75a69d707f9605c5e5358a397
bercik999 <bercik999@gmail.com>
2015-01-03 20:41:20 +0100

    bootstrap cdn

--- index.php
+++ index.php
@@ -53,10 +53,10 @@ $result = "Counted π: $pi based on ".throws_count." throws.";
 <!doctype html>
 <html lang="en">
 <head>
-  <link media="all" type="text/css" rel="stylesheet" href="http://176.58.111.46/packages/bootstrap/css/bootstrap.min.css">
+  <link media="all" type="text/css" rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
   <meta charset="UTF-8">
   <title>Calculate π by throwing hotdogs</title>
-</head>
+</head>src="<?php echo $image->output() ?>"
 <body>
   <div class="container">
     <div class="row clearfix">
